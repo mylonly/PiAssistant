b3e527e54228e48e825ae0199b02ff9519a5b353
Mylonly <dim@mylonly.com>
2016-05-19 14:45:26 +0800

    修改时间

--- src/clockSpeaker.py
+++ src/clockSpeaker.py
@@ -3,5 +3,6 @@ import time
 import ctypes
 
 so = ctypes.CDLL("../tts/libtts.so")
-timestr = time.strftime('%Y-%m-%d %X', time.localtime( time.time() ) )
-so.text2wav(timestr)
+timestr = time.strftime('%H:%M', time.localtime( time.time() ) )
+
+so.text2wav("现在时间:"+timestr)
